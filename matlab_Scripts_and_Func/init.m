30d619f11a830fa784a67edf0b245328962de8c0
DavidMCerdeira <david_cerdeira11@hotmail.com>
2015-12-22 16:43:12 +0000

    Updated scritps to use for loops

--- matlab_Scripts_and_Func/init.m
+++ matlab_Scripts_and_Func/init.m
@@ -1,107 +1,12 @@
-lp = fir1(8, 0.1);
-%%{
-gesture = 'pictures';
+cd E:\Universidade\MESTRADO_EEIC\ProjectoI\GMote;
 
-cd E:\Universidade\MESTRADO_EEIC\ProjectoI\GMote
+gesture = {'pictures'; 'video';  'music'; 'settings'; 'play_pause'; 'next'; 'previous'};
+number_of_samples = 9;
+number_of_gestures = length(gesture);
 
-circ_gest1 = getDataFromFile(gesture, 1);
-circ_gest2 = getDataFromFile(gesture, 2);
-circ_gest3 = getDataFromFile(gesture, 3);
-circ_gest4 = getDataFromFile(gesture, 4);
-circ_gest5 = getDataFromFile(gesture, 5);
-circ_gest6 = getDataFromFile(gesture, 6);
-circ_gest7 = getDataFromFile(gesture, 7);
-circ_gest8 = getDataFromFile(gesture, 8);
-circ_gest9 = getDataFromFile(gesture, 9);
+for k = 1 : number_of_gestures
+    for i = 1 : number_of_samples
+        sample{k, i} = getDataFromFile(gesture{k}, i);
+    end
+end
 %circ_gest10 = getDataFromFile(gesture, 10);
-%{
-circ_gest1 = filter(lp, 1, circ_gest1);
-circ_gest2 = filter(lp, 1, circ_gest2);
-circ_gest3 = filter(lp, 1, circ_gest3);
-circ_gest4 = filter(lp, 1, circ_gest4);
-circ_gest5 = filter(lp, 1, circ_gest5);
-circ_gest6 = filter(lp, 1, circ_gest6);
-circ_gest7 = filter(lp, 1, circ_gest7);
-circ_gest8 = filter(lp, 1, circ_gest8);
-circ_gest9 = filter(lp, 1, circ_gest9);
-%}
-%}
-% %{
-gesture = 'video';
-vi_gest1 = getDataFromFile(gesture, 1);
-vi_gest2 = getDataFromFile(gesture, 2);
-vi_gest3 = getDataFromFile(gesture, 3);
-vi_gest4 = getDataFromFile(gesture, 4);
-vi_gest5 = getDataFromFile(gesture, 5);
-vi_gest6 = getDataFromFile(gesture, 6);
-vi_gest7 = getDataFromFile(gesture, 7);
-vi_gest8 = getDataFromFile(gesture, 8);
-vi_gest9 = getDataFromFile(gesture, 9);
-%vi_gest10 = getDataFromFile(vi_gesture, 10);
-%{
-vi_gest1 = filter(lp, 1, vi_gest1);
-vi_gest2 = filter(lp, 1, vi_gest2);
-vi_gest3 = filter(lp, 1, vi_gest3);
-vi_gest4 = filter(lp, 1, vi_gest4);
-vi_gest5 = filter(lp, 1, vi_gest5);
-vi_gest6 = filter(lp, 1, vi_gest6);
-vi_gest7 = filter(lp, 1, vi_gest7);
-vi_gest8 = filter(lp, 1, vi_gest8);
-vi_gest9 = filter(lp, 1, vi_gest9);
-%}
-
-gesture = 'music';
-mu_gest1 = getDataFromFile(gesture, 1);
-mu_gest2 = getDataFromFile(gesture, 2);
-mu_gest3 = getDataFromFile(gesture, 3);
-mu_gest4 = getDataFromFile(gesture, 4);
-mu_gest5 = getDataFromFile(gesture, 5);
-mu_gest6 = getDataFromFile(gesture, 6);
-mu_gest7 = getDataFromFile(gesture, 7);
-mu_gest8 = getDataFromFile(gesture, 8);
-mu_gest9 = getDataFromFile(gesture, 9);
-
-gesture = 'settings';
-se_gest1 = getDataFromFile(gesture, 1);
-se_gest2 = getDataFromFile(gesture, 2);
-se_gest3 = getDataFromFile(gesture, 3);
-se_gest4 = getDataFromFile(gesture, 4);
-se_gest5 = getDataFromFile(gesture, 5);
-se_gest6 = getDataFromFile(gesture, 6);
-se_gest7 = getDataFromFile(gesture, 7);
-se_gest8 = getDataFromFile(gesture, 8);
-se_gest9 = getDataFromFile(gesture, 9);
-
-gesture = 'play_pause';
-pp_gest1 = getDataFromFile(gesture, 1);
-pp_gest2 = getDataFromFile(gesture, 2);
-pp_gest3 = getDataFromFile(gesture, 3);
-pp_gest4 = getDataFromFile(gesture, 4);
-pp_gest5 = getDataFromFile(gesture, 5);
-pp_gest6 = getDataFromFile(gesture, 6);
-pp_gest7 = getDataFromFile(gesture, 7);
-pp_gest8 = getDataFromFile(gesture, 8);
-pp_gest9 = getDataFromFile(gesture, 9);
-
-gesture = 'next';
-nx_gest1 = getDataFromFile(gesture, 1);
-nx_gest2 = getDataFromFile(gesture, 2);
-nx_gest3 = getDataFromFile(gesture, 3);
-nx_gest4 = getDataFromFile(gesture, 4);
-nx_gest5 = getDataFromFile(gesture, 5);
-nx_gest6 = getDataFromFile(gesture, 6);
-nx_gest7 = getDataFromFile(gesture, 7);
-nx_gest8 = getDataFromFile(gesture, 8);
-nx_gest9 = getDataFromFile(gesture, 9);
-
-gesture = 'previous';
-pr_gest1 = getDataFromFile(gesture, 1);
-pr_gest2 = getDataFromFile(gesture, 2);
-pr_gest3 = getDataFromFile(gesture, 3);
-pr_gest4 = getDataFromFile(gesture, 4);
-pr_gest5 = getDataFromFile(gesture, 5);
-pr_gest6 = getDataFromFile(gesture, 6);
-pr_gest7 = getDataFromFile(gesture, 7);
-pr_gest8 = getDataFromFile(gesture, 8);
-pr_gest9 = getDataFromFile(gesture, 9);
-
--- matlab_Scripts_and_Func/train.m
+++ matlab_Scripts_and_Func/train.m
@@ -4,228 +4,33 @@ n_sates = 4;
 count = 1;
 
 % %{ 
-gesture = 'pictures';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    circ_gest1;
+for i = 1 : number_of_gestures
+    %vector quantization
+    [m, ~, ~] = vqsplit([
+        sample{i,1};
+        
+        sample{i,2};
+        %{
+        sample{i,3};
+        
+        sample{i,4};
+        
+        sample{i,5};
+        
+        sample{i,6};
+        
+        sample{i,7};
+        
+        sample{i,8};
+        %}
+        ]', codebookSize);
     
-    circ_gest2;
-    
-    circ_gest3;
-    %{
-    circ_gest4;
+    md = KDTreeSearcher(m'); % grow a kd-tree
 
-    circ_gest5;
-
-    circ_gest6;
-
-    circ_gest7;
-
-    circ_gest8;
-
-    circ_gest9;
-    %}
-    ]', codebookSize);
-md = KDTreeSearcher(m'); % grow a kd-tree
-
-h(count) = HMM(gesture, n_sates, codebookSize, md, 75);
-idx = knnsearch( h(count).codebook, circ_gest1 ); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'video';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %vi_gest1;
-    
-    %vi_gest2;
-    
-    %vi_gest3;
-    
-    %vi_gest4;
-
-    vi_gest5;
-%{
-    vi_gest6;
-
-    vi_gest7;
-
-    vi_gest8;
-
-    vi_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, vi_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'music';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %mu_gest1;
-    
-    %mu_gest2;
-    
-    %mu_gest3;
-    
-    %mu_gest4;
-
-    mu_gest5;
-%{
-    mu_gest6;
-
-    mu_gest7;
-
-    mu_gest8;
-
-    mu_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, mu_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'settings';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %se_gest1;
-    
-    %se_gest2;
-    
-    %se_gest3;
-    
-    %se_gest4;
-
-    se_gest5;
-%{
-    se_gest6;
-
-    se_gest7;
-
-    se_gest8;
-
-    se_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, se_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'play_pause';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %pp_gest1;
-    
-    %pp_gest2;
-    
-    %pp_gest3;
-    
-    %pp_gest4;
-
-    pp_gest5;
-%{
-    pp_gest6;
-
-    pp_gest7;
-
-    pp_gest8;
-
-    pp_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, pp_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'next';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %nx_gest1;
-    
-    %nx_gest2;
-    
-    %nx_gest3;
-    
-    %nx_gest4;
-
-    nx_gest5;
-%{
-    nx_gest6;
-
-    nx_gest7;
-
-    nx_gest8;
-
-    nx_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, nx_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
-
-% %{
-gesture = 'previous';
-%vector quantization
-[m, ~, ~] = vqsplit([
-    %pr_gest1;
-    
-    %pr_gest2;
-    
-    %pr_gest3;
-    
-    %pr_gest4;
-
-    pr_gest5;
-%{
-    pr_gest6;
-
-    pr_gest7;
-
-    pr_gest8;
-
-    pr_gest9;
-    %}
-    ]', codebookSize);
-
-md1 = KDTreeSearcher(m'); % grow a kd-tree
-h(count) = HMM(gesture, n_sates, codebookSize, md1, 30);
-idx = knnsearch(h(count).codebook, pr_gest7); % match points with index
-fprintf('Training %s\n', gesture);
-h(count).train_one(idx);
-count = count + 1;
-%}
+    h(i) = HMM(gesture{i}, n_sates, codebookSize, md, 30);
+    idx = knnsearch( h(i).codebook, sample{i,1}); % match points with index
+    fprintf('Training %s\n', gesture{i});
+    h(i).train_one(idx);
+end
 fprintf('Done!\n');
-
 test
